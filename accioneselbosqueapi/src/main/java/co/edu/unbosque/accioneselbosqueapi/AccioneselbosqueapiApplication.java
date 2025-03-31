c2a41307345656c4f1452f1a5094fde8f6a0485e
vhincapie <vivianahincapie1610@gmail.com>
2025-03-31 12:09:27 -0500

    VH-ELB: Commit inicial

---
+++ accioneselbosqueapi/src/main/java/co/edu/unbosque/accioneselbosqueapi/AccioneselbosqueapiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.accioneselbosqueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccioneselbosqueapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccioneselbosqueapiApplication.class, args);
+	}
+
+}
